1004b3d9d3c6280a840c97746844ee832817d18f
aliouMarto <aliou.diaby@hetic.net>
2018-10-17 01:13:17 +0200

    dossier asset
    asset

---
+++ assets/js/regex.js
@@ -0,0 +1,16 @@
+// var nom=document.querySelector('.nom');
+// var mail=document.querySelector('.mail');
+// var msg=document.querySelector('.msg');
+// var envoi=document.querySelector('.envoi');
+
+// var test_nom=/^[a-zA-Z]+$/;
+// var test_msg=/^[a-zA-Z]+$/;
+// envoi.addEventListener('click',function(e){
+//   if(test_nom.test(nom) && test_msg.test(msg)){
+//   	alert('good');
+//   }else{
+//   	alert('no no');
+//   	e.preventDefault();
+//   }
+
+// })
